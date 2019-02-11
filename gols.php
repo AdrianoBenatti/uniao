418bb7d628d16540f3c6937f55e7961f47b7d0be
thisdedd <thisdedd@gmail.com>
2019-02-11 21:56:58 -0200

    Revert "Revert "Primeira marge""
    This reverts commit f8471df1337f1276318c297b664ab7434cf0e43d.

---
+++ gols.php
@@ -0,0 +1,29 @@
+<script>
+
+$(function () {
+		$('#number').each(function () {
+			$(this).prop('Counter', 0).animate({
+				Counter: $(this).text()
+			}, {
+				duration: 15000,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
+	});
+
+</script>
+
+
+<div class="ultimo-jogo">
+	<div class="centraliza">
+
+		<div class="contador reveal" id="contador" data-delay="400">
+			<p>Gols</p>
+			<div id="number">300</div>
+		</div>
+
+	</div>
+</div>
